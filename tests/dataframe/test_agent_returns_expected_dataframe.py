85ea6c97cdd73ca38e66a03808310bdf4a202c25
Aleksei Solovev <solovevstats@gmail.com>
2025-05-20 20:57:51 +0200

    Add unit test for agent response

--- tests/dataframe/test_agent_returns_expected_dataframe.py
+++ tests/dataframe/test_agent_returns_expected_dataframe.py
@@ -1,9 +1,25 @@
 import pandas as pd
+from langchain_core.messages import HumanMessage, SystemMessage
 
 from src.agent import create_agent_graph, get_dataframe_schema, get_system_prompt
 
 
 def test_agent_returns_expected_dataframe():
+
     df = pd.read_csv("tests/data/iris.csv")
-    assert len(df)
+
+    agent = create_agent_graph(df)
+    dataframe_schema = get_dataframe_schema(df)
+    system_prompt = get_system_prompt(dataframe_schema)
+
+    initial_state = {"messages": [SystemMessage(content=system_prompt)]}
+    user_input = "What is the average PetalWidthCm for each species?"
+    messages = initial_state["messages"] + [HumanMessage(content=user_input)]
+
+    target = df.groupby("Species")["PetalWidthCm"].mean()
+
+    response = agent.invoke({"messages": messages})
+    output = response["messages"][-1].additional_kwargs["dataframe"]
+
+    assert target.equals(output)
     
