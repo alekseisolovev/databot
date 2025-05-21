86614c940294dacf6feda07d9053d788cfe11489
Aleksei Solovev <solovevstats@gmail.com>
2025-05-21 09:28:32 +0200

    Add test_agent_returns_expected_series unit test

--- tests/dataframe/test_agent_returns_expected_artifacts.py
+++ tests/dataframe/test_agent_returns_expected_artifacts.py
@@ -4,6 +4,26 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from src.agent import create_agent_graph, get_dataframe_schema, get_system_prompt
 
 
+def test_agent_returns_expected_series():
+
+    df = pd.read_csv("tests/data/iris.csv")
+
+    agent = create_agent_graph(df)
+    dataframe_schema = get_dataframe_schema(df)
+    system_prompt = get_system_prompt(dataframe_schema)
+
+    target = df.groupby("Species")["PetalWidthCm"].mean()
+
+    initial_state = {"messages": [SystemMessage(content=system_prompt)]}
+    user_input = "What is the average PetalWidthCm for each species?"
+    messages = initial_state["messages"] + [HumanMessage(content=user_input)]
+
+    response = agent.invoke({"messages": messages})
+    output = response["messages"][-1].additional_kwargs["dataframe"]
+
+    pd.testing.assert_series_equal(target, output)
+
+
 def test_agent_returns_expected_dataframe():
 
     df = pd.read_csv("tests/data/iris.csv")
@@ -12,14 +32,13 @@ def test_agent_returns_expected_dataframe():
     dataframe_schema = get_dataframe_schema(df)
     system_prompt = get_system_prompt(dataframe_schema)
 
-    initial_state = {"messages": [SystemMessage(content=system_prompt)]}
-    user_input = "What is the average PetalWidthCm for each species?"
-    messages = initial_state["messages"] + [HumanMessage(content=user_input)]
+    target = df.describe()
 
-    target = df.groupby("Species")["PetalWidthCm"].mean()
+    initial_state = {"messages": [SystemMessage(content=system_prompt)]}
+    user_input = "What is the statistical summary of the dataset?"
+    messages = initial_state["messages"] + [HumanMessage(content=user_input)]
 
     response = agent.invoke({"messages": messages})
     output = response["messages"][-1].additional_kwargs["dataframe"]
 
-    assert target.equals(output)
-    
+    pd.testing.assert_frame_equal(target, output)
